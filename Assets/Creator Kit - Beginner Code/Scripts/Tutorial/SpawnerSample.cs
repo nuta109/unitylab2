c12b9d9d539382328b0a1eec0ecb839640e1ae81
nuta109 <58943138+nuta109@users.noreply.github.com>
2021-01-04 09:43:20 +0700

    short code

--- Assets/Creator Kit - Beginner Code/Scripts/Tutorial/SpawnerSample.cs
+++ Assets/Creator Kit - Beginner Code/Scripts/Tutorial/SpawnerSample.cs
@@ -7,35 +7,18 @@ public class SpawnerSample : MonoBehaviour
 
     void Start()
     {
-        int angle = 15;
-        int radius = 5;
-        Vector3 spawnPosition = transform.position;
-
-        Vector3 direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
-        spawnPosition = transform.position + direction * radius;
-        Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
-
-        angle = 55;
-        direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
-        spawnPosition = transform.position + direction * radius;
-        Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
-
-        angle = 95;
-        direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
-        spawnPosition = transform.position + direction * radius;
-        Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
-
-        // Fourth potion created at spawn
-        angle = 135;
-        direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
-        spawnPosition = transform.position + direction * radius;
-        Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
+        SpawnPotion(0);
+        SpawnPotion(45);
+        SpawnPotion(90);
+        SpawnPotion(135);
     }
-    
+
     void SpawnPotion(int angle)
     {
+        int radius = 5;
+
         Vector3 direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
-        spawnPosition = transform.position + direction * radius;
+        Vector3 spawnPosition = transform.position + direction * radius;
         Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
     }
 }
