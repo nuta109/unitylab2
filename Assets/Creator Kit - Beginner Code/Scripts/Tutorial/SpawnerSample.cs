dd25b931bbb966ec2567066273719999f079a784
nuta109 <58943138+nuta109@users.noreply.github.com>
2021-01-04 09:32:43 +0700

    Create your own variable

--- Assets/Creator Kit - Beginner Code/Scripts/Tutorial/SpawnerSample.cs
+++ Assets/Creator Kit - Beginner Code/Scripts/Tutorial/SpawnerSample.cs
@@ -8,20 +8,21 @@ public class SpawnerSample : MonoBehaviour
     void Start()
     {
         int angle = 15;
+        int radius = 5;
         Vector3 spawnPosition = transform.position;
 
         Vector3 direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
-        spawnPosition = transform.position + direction * 2;
+        spawnPosition = transform.position + direction * radius;
         Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
 
         angle = 55;
         direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
-        spawnPosition = transform.position + direction * 2;
+        spawnPosition = transform.position + direction * radius;
         Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
 
         angle = 95;
         direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
-        spawnPosition = transform.position + direction * 2;
+        spawnPosition = transform.position + direction * radius;
         Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
     }
 }
